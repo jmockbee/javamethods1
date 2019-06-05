0c30e84b2a8f9e594d1276efb1c52816771368e3
jmockbee <mockbee1000@gmail.com>
2019-06-05 18:23:23 -0400

    starting new method

--- src/com/JonMockbee/Main.java
+++ src/com/JonMockbee/Main.java
@@ -4,34 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
 
-      calculateScore();
+        calculateScore(true, 800, 5, 100);
 
-        // just spacing out code getting rid of data types
-        gameOver = true;
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
+        calculateScore(true, 10000, 8, 200);
 
-        if (gameOver == true) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score " + finalScore);
-        }
     }
 
-    // new method
-    public static void calculateScore() {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
 
         if (gameOver == true) {
             int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
             System.out.println("Your final score " + finalScore);
         }
 
